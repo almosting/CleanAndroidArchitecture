b0832e0939808106ec6d47cbfd72143456edb6d2
w.feng <w.feng@sunmedia.com.cn>
2022-07-05 17:31:18 +0800

    update exercise demo

---
+++ exercise0203/src/main/java/com/fengwei23/exercise0203/MainViewModel.kt
@@ -0,0 +1,28 @@
+package com.fengwei23.exercise0203
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+
+class MainViewModel(private val userService: UserService) : ViewModel() {
+
+    var resultState by mutableStateOf<List<User>>(emptyList())
+        private set
+
+    init {
+        viewModelScope.launch {
+            val users = userService.getUsers()
+            resultState = users
+        }
+    }
+}
+
+class MainViewModelFactory : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T =
+        MainViewModel(MyApplication.userService) as T
+}
