d3f51dbf99260d4c9f3ceefa454df459feffed27
w.feng <w.feng@sunmedia.com.cn>
2022-08-12 17:53:29 +0800

    update exercise demo

--- exercise0701/src/main/java/com/fengwei23/exercise0701/data_repository/repository/InteractionRepositoryImpl.kt
+++ exercise0701/src/main/java/com/fengwei23/exercise0701/data_repository/repository/InteractionRepositoryImpl.kt
@@ -3,6 +3,7 @@ package com.fengwei23.exercise0701.data_repository.repository
 import com.fengwei23.exercise0701.data_repository.data_source.local.LocalInteractionDataSource
 import com.fengwei23.exercise0701.domain.entity.Interaction
 import com.fengwei23.exercise0701.domain.repository.InteractionRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flow
@@ -14,6 +15,7 @@ class InteractionRepositoryImpl @Inject constructor(
 
     override fun getInteraction(): Flow<Interaction> = interactionDataSource.getInteraction()
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     override fun saveInteraction(interaction: Interaction): Flow<Interaction> = flow {
         interactionDataSource.saveInteraction(interaction)
         this.emit(Unit)
